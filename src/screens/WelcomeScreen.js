6206cf45bb3de9da7074e3870eaf21d6096966aa
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-11-27 01:38:43 +0600

    home screen edited for writing posts

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -9,29 +9,96 @@ import {
   Input,
   Header} from 'react-native-elements';
 import { StatusBar } from "expo-status-bar";
+import { FontAwesome5 } from '@expo/vector-icons';
+import { storeDataJSON } from "../functions/AsnycStorageFunctions";
+import { color } from "react-native-reanimated";
+import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
 
 
-const WelcomeScreen = (props)=>{
+const [post, setPost] = useState([]);
+const [user, setUser] = useState([]);
+
+function ShowCurrentDate() {
+  var date = new Date().getDate();
+  var month = new Date().getMonth() + 1;
+  var year = new Date().getFullYear();
+  return (date + "/" + month + "/" + year);
+}
+
+const PostWriting = ({user}) => {
+  return (
+    <Card>
+      <Input
+        placeholder = "What's in your mind?"
+        multiline = {true}
+        leftIcon = 
+         {<FontAwesome5 name="pen-fancy" size={24} color="black" />}
+        onChangeText = { (text) => {
+          setPost(text);
+        }
+
+        }
+      />
+
+      <View>
+        <Button
+          title = "Post" buttonStyle = {{width : 80, alignSelf : "flex-start"}}
+          onPress = {() => {
+            let newPost = {
+              post : post,
+              user : user.name,
+              date : ShowCurrentDate()
+            }
+            storeDataJSON(Email, newPost);
+          }}
+        />
+        <Button style = {styles.buttonStyle}
+          disabled = {true}
+          type = "outline"
+          
+        />
+
+      </View>
+    </Card>
+  )
+}
+
+
+const WelcomeScreen = ({navigation})=>{
   return(
     <AuthContext.Consumer> 
       {(auth) => 
-        (<View style = {styles.viewStyle}>
-          <StatusBar
-            hidden = {true}
-            backgroundColor = "blue"
-            barStyle = "light-content"
+        (<View>
+          <StatusBar />
+          <Header
+            containerStyle = {{
+              backgroundColor : "#939fcf",
+              justifyContent : "space-around",
+            }}
+            leftComponent = {
+              <AntDesign name="menuunfold" size={24} color="white" 
+               onPress = {() => {
+                  navigation.openDrawer();
+               }}/>
+            }
+            centerComponent = {{text : "Home", style: {color : "#ffffff"}}}
+            rightComponent = 
+            {
+              <MaterialCommunityIcons name="logout-variant" size={24} color="white" 
+              onPress = {function(){
+                auth.setIsLoggedIn(false);
+                auth.setCurrentUser({});
+             
+              }}
+              />
+            }
           />
+         
           <Text style = {styles.TextStyle}>Welcome {auth.currentUser.name}</Text>
 
-          <Button
-            type = "solid"
-            title = "Log Out"
-            onPress = {function(){
-               auth.setIsLoggedIn(false);
-               auth.setCurrentUser({});
+          <PostWriting user = {auth.currentUser} />
             
-            }}
-          />
+          
         </View>)}
     </AuthContext.Consumer>
   );
@@ -49,6 +116,11 @@ const styles = StyleSheet.create(
       fontSize: 25,
       color: "black",
     },
+    buttonStyle : {
+      width : 100,
+      alignSelf : "flex-end",
+      color : "blue",    
+    }
   } 
 
 );

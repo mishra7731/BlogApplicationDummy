ac6d0a454e71db1d1cda8baa95d567ee74c8b770
syedamishra@iut-dhaka.edu <syedamishra@iut-dhaka.edu>
2020-11-26 22:03:31 +0600

    Drawer tab added, App.js only modified

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+
+import {Stylesheet, Text, View} from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
+import {createDrawerNavigator} from "@react-navigation/drawer";
+import {createMaterialBottomTabNavigator} from "@react-navigation/material-bottom-tabs";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
@@ -8,14 +14,47 @@ import SignInScreen from './src/screens/SignInScreen';
 import NotificationScreen from "./src/screens/NotificationScreen";
 import {AuthContext, AuthProvider} from "./src/provider/AuthProvider";
 
-const WelcomeStack = createStackNavigator();
-const AuthStack = createStackNavigator();
 
-const WelcomeStackScreen = () => { 
+const AuthStack = createStackNavigator();
+const WelcomeTab = createMaterialBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerScreens = () => {
   return(
-    <WelcomeStack.Navigator initialRouteName = 'Welcome'>
-      <WelcomeStack.Screen name = 'Welcome' component = {WelcomeScreen}/>
-    </WelcomeStack.Navigator>
+    <Drawer.Navigator initialRouteName = "Welcome">
+      <Drawer.Screen name = "Welcome" component = {WelcomeScreenTab} />
+    </Drawer.Navigator>
+  );
+}
+
+const WelcomeScreenTab = () => { 
+  return(
+    <WelcomeTab.Navigator 
+      initialRouteName = "Welcome"
+      activeColor = "#030659"
+      shifting = {true}>
+      <WelcomeStack.Screen name = "Welcome" 
+        component = {WelcomeScreen}
+        options = {{
+          tabBarLabel : "Welcome",
+          tabBarColor : "#1a8bab",
+          tabBarIcon : ({color}) => (
+            <MaterialCommunityIcons name="home-heart" size={24} color="black" />
+          ),
+        }}
+      />
+      <WelcomeTab.Screen name = "Notification"
+        component = {NotificationScreen}
+        options = {{
+          tabBarLabel : "Notification",
+          tabBarColor : "#067bc9",
+          tabBarIcon : ({color}) => (
+            <Ionicons name="md-notifications" size={24} color="black" />
+          ),
+
+        }}
+      />
+    </WelcomeTab.Navigator>
   );
 };
 
@@ -44,7 +83,7 @@ function App(){
       <AuthContext.Consumer>
         {(auth) => (
         <NavigationContainer> 
-          {auth.IsLoggedIn ? <WelcomeStackScreen /> : <AuthStackScreen />}
+          {auth.IsLoggedIn ? <DrawerScreens /> : <AuthStackScreen />}
         </NavigationContainer>)}
       </AuthContext.Consumer> 
     </AuthProvider>
